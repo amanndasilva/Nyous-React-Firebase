73601d5ca0192044d25ecb43a9c58ba7d4bc922f
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-12-11 20:27:50 -0200

    Aula 3 - finalizada

---
+++ nyous-react-firebase/src/pages/login/index.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { useFirebaseApp } from 'reactfire';
+
+import { Container, Form, Button } from 'react-bootstrap';
+import './index.css';
+//import logo from '../../assets/img/';
+
+const Login = () => {
+
+    const firebase = useFirebaseApp();
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const logar = (event) => {
+        event.preventDefault();
+
+        console.log('${email} - ${senha}');
+
+        firebase.auth().signInWithEmailAndPassword(email, senha)
+        .then(result => {
+            //navegam para a página
+            localStorage.setToken('nyous', result.user.refreshToken);
+            alert('Seja ben vindo');
+        })
+        .catch(error => {
+            alert('Email ou senha inválidos');
+            console.error(error);
+        })
+    }
+
+    return(
+        <Container>
+            <Form className='form-signin' onSubmit={event => logar(event)}>
+                
+                <br/>
+                <small>Informe seus dados abaixo</small>
+                <hr/>
+
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Informe o email" value={email} onChange={event => setEmail(event.target.value)} required />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control type="password" placeholder="Senha" value={senha} onChange={event => setSenha(event.target.value)} required/>
+                </Form.Group>
+
+                <Button variant="primary" type="submit" >
+                    Enviar
+                </Button>
+
+                <br/><br/>
+                <a href='/login' style={{marginTop :'30px'}}>Ainda não tenho conta!</a>
+            </Form>
+        </Container>
+    )
+}
+
+export default Login;
+
+//<div className='text-center'>
+//<img src={logo} alt='NYOUS' style={{ width : '64px'}} />
+//</div>
---
+++ nyous-react-firebase/src/utils/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCLmlOFaweQ2DsDQ_86rkuEtXLqrQGkx8I",
+    authDomain: "nyous-a245e.firebaseapp.com",
+    projectId: "nyous-a245e",
+    storageBucket: "nyous-a245e.appspot.com",
+    messagingSenderId: "116699014642",
+    appId: "1:116699014642:web:157dd5a4e63f48e10cb76b",
+    measurementId: "G-P4VFSPKRRG"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
